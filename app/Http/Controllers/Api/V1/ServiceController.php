9a40906089b92f1b8c3099a5bfbd76ebace66c47
Cihan Alp <extern.cihan.alp@tttech-auto.com>
2022-02-17 08:56:01 +0300

    services cached

--- app/Http/Controllers/Api/V1/ServiceController.php
+++ app/Http/Controllers/Api/V1/ServiceController.php
@@ -5,11 +5,16 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ServiceCollection;
 use App\Models\Service;
+use Illuminate\Support\Facades\Cache;
 
 class ServiceController extends Controller
 {
     public function index()
     {
-        return new ServiceCollection(Service::all());
+    	$services = Cache::remember('orders', 3600*24*30, function () {
+            return Service::all();
+        });
+        
+        return new ServiceCollection($services);
     }
 }
